2f0c422a8a6fecda152b35e5dc5961f3c0d84e81
Vincent <voltion@influxdata.com>
2023-05-02 10:50:31 -0600

    Add additional layout

--- rc.lua
+++ rc.lua
@@ -362,6 +362,7 @@ local layouts =
     awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
+    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
